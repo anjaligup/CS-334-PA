1aaf123f8e8ce15685557debc71deece4b6bbbfb
Anjali Gupta <134241823+anjaligup@users.noreply.github.com>
2025-12-23 11:21:58 -0500

    Add files via upload

--- task1.py
+++ task1.py
@@ -1,70 +1,43 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
+import sys
 
-import random 
-import sys 
-
-#read code from STDIN
 def read_from_stdin():
-    states = input()
-    states = eval(states)
+    terminals = sys.stdin.readline()
+    terminals = eval(terminals)
 
-    sigma = sys.stdin.readline()
-    sigma = eval(sigma)
+    variables = sys.stdin.readline()
+    variables = eval(variables)
 
-    dlt = sys.stdin.readline()
-    dlt = eval(dlt)
+    productions = sys.stdin.readline()
+    productions = eval(productions)
 
-    qs = sys.stdin.readline()
-    qs = eval(qs)
-
-    F = sys.stdin.readline()
-    F = eval(F)
+    string = sys.stdin.readline()
+    string = eval(string)
     
-    symbol = sys.stdin.readline()
-    symbol = eval(symbol)
+    index = sys.stdin.readline()
+    index = eval(index)
     
-    return states, sigma, dlt, qs, F, symbol
+    return terminals, variables, productions, string, index
 
+#read variables from stdin
+terminals, variables, productions, string, index = read_from_stdin()
 
-# create empty matrix given rows + columns 
-def create_matrix_of_zeros(rows, cols):
-    M = []
-    for s in range(rows):
-        M.append([])
-        for r in range(cols):
-            M[s].append(0)
-    return M
+def apply_single_production_left(string, index):    
+    # find leftmost variable in the string (aka the first variable in string)
+    var = None 
+    for character in string:
+        if character in variables:
+            var = character
+            break
+            
+    if var is None:
+        print(string)
+    else:
+        # use index to find the production that needs to be used
+        prod = productions[var][index]
 
-# print the above matrix 
-def print_matrix(A, padding):
-    rows = len(A)
-    cols = len(A[0])
-    for i in range(rows):
-        for j in range(cols):
-            if j < cols-1:
-                print (f"{A[i][j]: {padding}}", end = " ")
-            else:
-                print (f"{A[i][j]: {padding}}")
-                
-                
-# main function 
-def create_matrix_of_transitions_over_a_symbol():
-    states, sigma, dlt, qs, F, symbol = read_from_stdin()
+        # replace the variable in the string with the production
+        new_string = string.replace(var, prod, 1)
+
+        print(new_string)
     
-    # figure out rows/columns by taking the length of the states list 
-    n = len(states)
-    
-    # initialize our matrix
-    M = create_matrix_of_zeros(n, n)
-    
-    # actually fill in the matrix 
-    for j in range(len(states)):
-        s = states[j]
-        if (s, symbol) in dlt:
-            for target in dlt[(s, symbol)]:
-                i = states.index(target)   # row index for target state
-                M[i][j] = 1.0
-                
-    print_matrix(M, 4)
-    
-create_matrix_of_transitions_over_a_symbol()
+apply_single_production_left(string, index)
